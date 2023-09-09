cbe3a583686554af73434e456185faabb832f9d4
Murad <83445039+Murad039@users.noreply.github.com>
2023-09-09 19:35:55 +0600

    Update endpoint.py
    done and dusted

--- endpoint.py
+++ endpoint.py
@@ -34,5 +34,5 @@ def get_info():
     }
     return jsonify(response_data)
 
-    if __name__ == '__main__':
-        app.run(host= '0.0.0.0', debug = True, port = 5000 )
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0', debug = True, port = 5000 )
